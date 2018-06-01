369277bac2d0100c62ad9cdd04067d8b1e19301b
vidhurraj147 <37847430+vidhurraj147@users.noreply.github.com>
2018-06-01 00:33:50 -0400

    Count of numbers

---
+++ HelloGit/src/net/ubilife/world/NumbersCount.java
@@ -0,0 +1,12 @@
+package net.ubilife.world;
+
+public class NumbersCount {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=100;i++) {
+			System.out.println("THIS IS I and Value "+i);
+		}
+
+	}
+
+}
